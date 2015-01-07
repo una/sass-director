e056b654c12c46df779007e03e066d59d09a7628
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-01-07 10:33:39 -0600

    a bit of js refactoring

--- scripts.js
+++ scripts.js
@@ -1,61 +1,58 @@
-var inputText = '',
-    extension = '.scss', //temp
-    lastDir = '',
-    currentDir = '',
-    currentFile = '',
-    finalOutput = '',
-    underscore = '_'; //blank if underscore is false
-
 // split a string to get text between before & after characters
-// Usage  var you = 'hello you guys'.between('hello ',' guys');
-// --> you = 'you';
-// via http://snipplr.com/view/14074/
-String.prototype.between = function(before, after) {
-  s = this;
-  var i = s.indexOf(before);
+// Usage  var word = 'the unicorns rule the world'.between('the ',' rule');
+// --> word = 'unicorns';
+function between(input, before, after) {
+  var i = input.indexOf(before);
   if (i >= 0) {
-    s = s.substring(i + before.length);
+    input = input.substring(i + before.length);
   }
   else {
     return '';
   }
   if (after) {
-    i = s.indexOf(after);
+    i = input.indexOf(after);
     if (i >= 0) {
-      s = s.substring(0, i);
+      input = input.substring(0, i);
     }
     else {
       return '';
     }
   }
-  return s;
+  return input;
 }
 
-function getInput() {
-  inputText = document.getElementById('input-text').value;
-  extension = document.querySelector('input[name="extension"]:checked').value;
-}
 
 function doTheThing() {
-  getInput();
-  //inputText = '@import "vendors/bootstrap";\n@import "vendors/jquery-ui";\n\n@import "utils/variables";\n@import "utils/functions";\n@import "utils/mixins";\n@import "utils/placeholders";';
+  var inputText = '',
+      extension = '.scss', //temp
+      lastDir = '',
+      currentDir = '',
+      currentFile = '',
+      finalOutput = '',
+      underscore = '_',
+      outputPlatter = document.getElementById('output-text'),
+      i; //blank if underscore is false
+
+  // Get the input here
+  inputText = document.getElementById('input-text').value;
+  extension = document.querySelector('input[name="extension"]:checked').value;
 
   var lines = inputText.split('\n'); //split up the lines in the string into a lines array
-    for(var i = 0; i < lines.length; i++) {
-      currentDir = lines[i].between('"', '/'); //get the directory this line refers to
-      currentFile = lines[i].between('/', '"'); //get file created in this line
+  for(var i = 0; i < lines.length; i++) {
+    currentDir = between(lines[i],'"', '/'); //get the directory this line refers to
+    currentFile = between(lines[i],'/', '"'); //get file created in this line
 
-      if (lines[i].charAt(0) === '@') { //skip blank lines & comments
-        if (currentDir === lastDir) { //if the current lines dir is the same as the last one
-          finalOutput += 'touch ' + underscore + currentFile + extension + ';';
-        }
-        else { //if current dir != last one
-          if (i > 0) { finalOutput += 'cd ../;' } //dont cd for the first line
-           finalOutput += 'mkdir ' + currentDir + ';cd ' + currentDir +';' + 'touch ' + underscore + currentFile + extension + ';';
-        }
-      lastDir = currentDir;
+    if (lines[i].charAt(0) === '@') { //skip blank lines & comments
+      if (currentDir === lastDir) { //if the current lines dir is the same as the last one
+        finalOutput += 'touch ' + underscore + currentFile + extension + ';';
       }
+      else { //if current dir != last one
+        if (i > 0) { finalOutput += 'cd ../;' } //dont cd for the first line
+         finalOutput += 'mkdir ' + currentDir + ';cd ' + currentDir +';' + 'touch ' + underscore + currentFile + extension + ';';
+      }
+    lastDir = currentDir;
     }
-    document.getElementById('output-text').value = finalOutput;
+  }
+  outputPlatter.value = finalOutput;
 }
 
