e01912cddcd909ee01214fa34d7d497033104278
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-01-06 21:10:23 -0600

    correct shell for making new directories & cding into them! :fireworks:

--- scripts.js
+++ scripts.js
@@ -1,37 +1,24 @@
 var inputText = '',
     extension = '.scss', //temp
-    workingDir = '';
+    lastDir = '',
+    workingFile = '',
+    finalOutput = '';
 
-function getInput() {
-  inputText = document.getElementById('input-text').value;
-}
-
-function doTheThing() {
-  getInput();
-
-  var lines = inputText.split('\n'); //split up the lines in the string into a lines array
-    for(var i = 0; i < lines.length; i++) {
-      workingDir = lines[i].between('"', '/');
-      console.log(lines[i], workingDir)
-    }
-}
-
-/**
- * Usage  var you = 'hello you guys'.between('hello ',' guys');
- * you = 'you';
- * via http://snipplr.com/view/14074/
- */
-String.prototype.between = function(prefix, suffix) {
+// split a string to get text between before & after characters
+// Usage  var you = 'hello you guys'.between('hello ',' guys');
+// --> you = 'you';
+// via http://snipplr.com/view/14074/
+String.prototype.between = function(before, after) {
   s = this;
-  var i = s.indexOf(prefix);
+  var i = s.indexOf(before);
   if (i >= 0) {
-    s = s.substring(i + prefix.length);
+    s = s.substring(i + before.length);
   }
   else {
     return '';
   }
-  if (suffix) {
-    i = s.indexOf(suffix);
+  if (after) {
+    i = s.indexOf(after);
     if (i >= 0) {
       s = s.substring(0, i);
     }
@@ -41,3 +28,46 @@ String.prototype.between = function(prefix, suffix) {
   }
   return s;
 }
+
+function getInput() {
+  inputText = document.getElementById('input-text').value;
+}
+
+function doTheThing() {
+  getInput();
+  inputText = '@import "vendors/bootstrap";\n@import "vendors/jquery-ui";\n\n@import "utils/variables";\n@import "utils/functions";\n@import "utils/mixins";\n@import "utils/placeholders";';
+
+  var lines = inputText.split('\n'); //split up the lines in the string into a lines array
+    for(var i = 0; i < lines.length; i++) {
+      currentDir = lines[i].between('"', '/');
+      if (lines[i] != '') { //skip blank lines
+        if (currentDir === lastDir) { //if the current lines dir is the same as the last one
+          console.log(lines[i], 'same lastDir', lastDir, lastDir.length);
+          //finalOutput += 'touch ' + workingFile + ';';
+        }
+        else {
+           finalOutput += 'mkdir ' + currentDir + ';cd ' + currentDir +';';
+           console.log(lines[i], 'not same', lastDir)
+        }
+      lastDir = currentDir;
+      }
+    }
+    console.log(finalOutput)
+}
+
+// Looking for this:
+// mkdir vendors; cd vendors; touch bootstrap.scss; touch jquery-ui.scss; cd ../;
+// mkdir utils; cd utils; touch variables.scss; touch functions.scss; touch mixins.scss
+//
+/* Out of:
+ @import "vendors/bootstrap";
+ @import "vendors/jquery-ui";
+
+ @import "utils/variables";
+ @import "utils/functions";
+ @import "utils/mixins";
+ @import "utils/placeholders";
+*/
+
+
+
