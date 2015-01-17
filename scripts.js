bf829a74553f2e473e651d6c76535b8b13861f72
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-01-17 00:48:36 -0600

    breaking up the input string into individual parts

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // split a string to get text between before & after characters
-//var i = 0;
+var i = 0;
 
 function between(input, before, after) {
-  i = input.indexOf(before);
+  var i = input.indexOf(before);
   if (i >= 0) {
     input = input.substring(i + before.length);
   }
@@ -21,33 +21,75 @@ function between(input, before, after) {
   return input;
 }
 
-var sample='@import "layout/navigation";\n@import "layout/meow/grid";\n@import "layout/meow/header";\n@import "layout/meow/2/footer";\n@import "layout/meow/2/sidebar";\n@import "layout/meow/2/forms";\n\n@import "components/buttons";\n@import "components/carousel";\n@import "components/cover";\n@import "components/dropdown";\n\n@import "pages/home";\n@import "pages/contact";\n\n@import "themes/theme";\n@import "themes/admin";';
-
-function doTheThing(inputText) {
-  var extension = '.scss', //default
-      finalObj = {},
-      quotationStyle = '"',
-      underscore = '_', //default
-      i;
-
-  // Get the input & options here
-  inputText = sample;
-  //inputText = document.getElementById('input-text').value;
-  extension = document.querySelector('input[name="extension"]:checked').value;
-  if (!document.querySelector('input[name="underscore"]').checked) {
-    underscore = '';
-  }
-
-  var lines = inputText.split('\n'); //split up the lines in the string into a lines array
-  for(i = 0; i < lines.length; i++) {
-    if (lines[i].charAt(0) === '@') { //skip blank lines & comments
-      quotationStyle = lines[i].charAt(8); //the first character after '@import ' is the quotation style
-
-      console.log(lines[i], underscore, extension);
-
-
+function nth_ocurrence(str, thing, nth) {
+  for (var i=0;i<str.length;i++) {
+    if (str.charAt(i) == thing) {
+        if (!--nth) {
+           return i;
+        }
     }
   }
+  return false;
+}
+
+//   if (currentDir === lastDir) {
+//     if (subDirCount === 0) { //just make the file
+//       finalOutput += 'touch ' + underscore + currentFile + extension + ';';
+//     }
+
+//     else { //if subDirCount > 0
+//       smallerStr = lines[i].replace(currentDir + '/',''); //remove the dir before
+//       currentDir = between(smallerStr, quotationStyle, '/');
+//       dirHist.push(lastDir +'/' + currentDir);
+//       currentFile = between(smallerStr,'/', quotationStyle);
+//       console.log('im a subdir!', currentDir, currentFile, dirHist);
+//       finalOutput += 'mkdir ' + currentDir + ';cd ' + currentDir +';';
+//       subDirCount--;
+//       addFiles();
+//     }
+//   }
+
+//   else { //if current dir != last one make & cd into dir
+//     if (i != 0 && subDirCount === 0) { finalOutput += 'cd ../;' } //also dont cd for the first line
+//     finalOutput += 'mkdir ' + currentDir + ';cd ' + currentDir +';'; //make the dir & cd into it
+//     lastDir = currentDir;
+//     dirHist.push(currentDir);
+//     addFiles()
+//   }
+// }
+
+var extension = '.scss', //default
+    finalObj = {},
+    quotationStyle = '"',
+    underscore = '_', //default
+    currentDir,
+    subdirs = 0;
+
+var sample='@import "layout/navigation";\n@import "layout/meow/grid";\n@import "layout/meow/header";\n@import "layout/meow/2/footer";\n@import "layout/meow/2/sidebar";\n@import "layout/meow/2/forms";\n\n@import "components/buttons";\n@import "components/carousel";\n@import "components/cover";\n@import "components/dropdown";\n\n@import "pages/home";\n@import "pages/contact";\n\n@import "themes/theme";\n@import "themes/admin";';
+
+function doTheThing() {
+  // Get the input & options here
+  var inputText = sample;
+  //inputText = document.getElementById('input-text').value;
+  extension = document.querySelector('input[name="extension"]:checked').value;
+  if (!document.querySelector('input[name="underscore"]').checked) { underscore = ''; }
+
+  var lines = inputText.split('\n'); //split up the lines in the string into a lines array
+
+  for(i = 0; i < lines.length; i++) {
+    subDirCount = ((lines[i]).match(/\//g) || []).length; //get # of subdirectories in the line
+
+    if (lines[i].charAt(0) === '@') { //skip blank lines & comments
+      var cutOut = lines[i].substr(9, lines[i].length-11).split('/');
+        for(var n=0; n<cutOut.length; n++) {
+          console.log(cutOut[n]);
+        }
+      //if its the last item in the array, its a file
+      //if its above that, its a subDir
+    }
+  }
+
+  //   // console.log(subDirCount);
 }
 
 doTheThing();
